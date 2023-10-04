6b80a3e0c0bc877a6ea30e16f033e7fe0b226a97
Shun <s.tsuchiya.business@gmail.com>
2023-10-05 01:17:20 +0900

    :construction: [work in progress] => dev-toolのinstall Scriptの作成・hello_worldプログラムの作成

--- hello_rust/target/debug/deps/hello_rust-174c3ed621f8cead.d
+++
@@ -1,5 +0,0 @@
-/Users/shunsuke.tsuchiya/Hobby/rust_playground/hello_rust/target/debug/deps/hello_rust-174c3ed621f8cead: src/main.rs
-
-/Users/shunsuke.tsuchiya/Hobby/rust_playground/hello_rust/target/debug/deps/hello_rust-174c3ed621f8cead.d: src/main.rs
-
-src/main.rs:
--- hello_rust/target/debug/hello_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/shunsuke.tsuchiya/Hobby/rust_playground/hello_rust/target/debug/hello_rust: /Users/shunsuke.tsuchiya/Hobby/rust_playground/hello_rust/src/main.rs
